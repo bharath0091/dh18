1f5b5e4c44852ee778750074302ab628ea406bc5
Abbin Varghese <Abbin.Varghese@klm.com>
2018-05-26 15:50:49 +0200

    updated controller

--- smart-crimereporter-web/src/main/java/com/police/innovation/web/CrimeReportController.java
+++ smart-crimereporter-web/src/main/java/com/police/innovation/web/CrimeReportController.java
@@ -6,15 +6,13 @@ import com.police.innovation.persistance.CrimeReportRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableGlobalMethodSecurity(securedEnabled = false)
+@RestController
 public class CrimeReportController {
     Logger LOG = LoggerFactory.getLogger(this.getClass().getName());
 
@@ -22,9 +20,9 @@ public class CrimeReportController {
     private CrimeReportRepo crimeReportRepo;
 
     @GetMapping(path = "/")
-    public String editTokenEndpoint(@ModelAttribute CrimeReport crimeReport) {
+    public CrimeReport editTokenEndpoint(@ModelAttribute CrimeReport crimeReport) {
         LOG.info("CrimeReport home page");
-        return "crimeReport";
+        return new CrimeReport();
     }
 
     @PostMapping(path = "/")
