f72cb6ea78e32cc41824dccb10a599045a9b14b1
Bharath Kumar Chennupati <bharath.chennupati@klm.com>
2018-05-27 10:43:16 +0200

    LocationResponse

---
+++ smart-crimereporter-web/src/main/java/com/police/innovation/model/kadaster/LocationResponse.java
@@ -0,0 +1,20 @@
+package com.police.innovation.model.kadaster;
+
+import com.police.innovation.model.Convict;
+import com.police.innovation.model.Officer;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+public class LocationResponse {
+	private Result result	;
+
+	
+	
+}
