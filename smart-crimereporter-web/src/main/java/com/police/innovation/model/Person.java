2c4ffe339deceaf1324882420203ecbb06e9a097
Sriram <sriram.sridharan@klm.com>
2018-05-27 09:45:53 +0200

    Added few more

--- smart-crimereporter-web/src/main/java/com/police/innovation/model/Person.java
+++ smart-crimereporter-web/src/main/java/com/police/innovation/model/Person.java
@@ -1,15 +1,35 @@
 package com.police.innovation.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Person {
-
 	private String name;
 	private String address;
-	private String age;
+	private int age;
 	private String gender;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	
 }
